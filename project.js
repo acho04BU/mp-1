cf5df3e97aa5e68426c9529f2d01afd123978c6d
Alex Cho <acho04@bu.edu>
2026-09-17 19:31:37 -0400

    added red text to negative results, also added code to change output to black at the start of each function to maintain black color on non-negative outputs

--- project.js
+++ project.js
@@ -5,46 +5,55 @@ let output = document.getElementById('output');
 
 function addition(){
     // we weren't taught how to typecast in class, so I decided to typecast using Number from W3Schools, and used the function isNaN to catch non valid inputs.
-
+    output.style.color = "black";
     if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
         output.innerHTML = "Invalid input(s)";
     }else{
-        output.innerHTML = Number(firstInput.value) + Number(secondInput.value);
+        let x = Number(firstInput.value) + Number(secondInput.value);
+        output.innerHTML = x;
+        if (x < 0) output.style.color = "red";
     }
 }
 
 function subtraction(){
-
+    output.style.color = "black";
     if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
         output.innerHTML = "Invalid input(s)";
     }else{
-        output.innerHTML = Number(firstInput.value) - Number(secondInput.value);
+        let x = Number(firstInput.value) - Number(secondInput.value);
+        output.innerHTML = x;
+        if (x < 0) output.style.color = "red";
     }
 }
 
 function multiplication(){
-
+    output.style.color = "black";
     if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
         output.innerHTML = "Invalid input(s)";
     }else{
-        output.innerHTML = Number(firstInput.value) * Number(secondInput.value);
+        let x = Number(firstInput.value) * Number(secondInput.value);
+        output.innerHTML = x;
+        if (x < 0) output.style.color = "red";
     }
 }
 
 function division(){
-
+    output.style.color = "black";
     if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
         output.innerHTML = "Invalid input(s)";
     }else if (Number(secondInput.value === 0)){
         output.innerHTML = "Divide by 0";
     }else{
-        output.innerHTML = Number(firstInput.value) / Number(secondInput.value);
+        let x = Number(firstInput.value) / Number(secondInput.value);
+        output.innerHTML = x;
+        if (x < 0) output.style.color = "red";
 
     }
 
 }
 
 function power(){
+    output.style.color = "black"; //reset color of font
     if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
         output.innerHTML = "Invalid input(s)";
     }else if (Number(secondInput.value >= 0)){
@@ -53,12 +62,14 @@ function power(){
             n *= Number(firstInput.value);
         }
         output.innerHTML = n;
+        if (n < 0) output.style.color = "red";
     }else{
         let n = 1;
         for (let i = 0; i > Number(secondInput.value); i--){
             n /= Number(firstInput.value);
         }
         output.innerHTML = n;
+        if (n < 0) output.style.color = "red";
     }
 }
 
