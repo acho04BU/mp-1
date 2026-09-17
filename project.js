c05571e629a8500eefffaaaa2fb4bf652ea4390f
Alex Cho <acho04@bu.edu>
2026-09-17 19:21:32 -0400

    working on calculator, clear() button doesn't seem to be working, trying to fix with console logs

---
+++ project.js
@@ -0,0 +1,71 @@
+let firstInput = document.getElementById('first-input');
+let secondInput = document.getElementById('second-input');
+
+let output = document.getElementById('output');
+
+function addition(){
+    // we weren't taught how to typecast in class, so I decided to typecast using Number from W3Schools, and used the function isNaN to catch non valid inputs.
+
+    if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
+        output.innerHTML = "Invalid input(s)";
+    }else{
+        output.innerHTML = Number(firstInput.value) + Number(secondInput.value);
+    }
+}
+
+function subtraction(){
+
+    if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
+        output.innerHTML = "Invalid input(s)";
+    }else{
+        output.innerHTML = Number(firstInput.value) - Number(secondInput.value);
+    }
+}
+
+function multiplication(){
+
+    if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
+        output.innerHTML = "Invalid input(s)";
+    }else{
+        output.innerHTML = Number(firstInput.value) * Number(secondInput.value);
+    }
+}
+
+function division(){
+
+    if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
+        output.innerHTML = "Invalid input(s)";
+    }else if (Number(secondInput.value === 0)){
+        output.innerHTML = "Divide by 0";
+    }else{
+        output.innerHTML = Number(firstInput.value) / Number(secondInput.value);
+
+    }
+
+}
+
+function power(){
+    if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
+        output.innerHTML = "Invalid input(s)";
+    }else if (Number(secondInput.value >= 0)){
+        let n = 1;
+        for (let i = 0; i < Number(secondInput.value); i++){
+            n *= Number(firstInput.value);
+        }
+        output.innerHTML = n;
+    }else{
+        let n = 1;
+        for (let i = 0; i > Number(secondInput.value); i--){
+            n /= Number(firstInput.value);
+        }
+        output.innerHTML = n;
+    }
+}
+
+function clear(){
+    console.log("AHHHHHHHH");
+    //reset from stack overflow: https://stackoverflow.com/questions/17237772/html-how-to-clear-input-using-javascript
+    document.getElementById("first-input").value = '';
+    secondInput.value= '';
+    console.log("AHHHH");
+}
