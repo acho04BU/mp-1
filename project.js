25186ae78415d6ae2320906dc11d1b8a3e367b8b
Alex Cho <acho04@bu.edu>
2026-09-17 20:00:33 -0400

    added jarvis / iron man, but noticed new issue where elements of the page get scrunched if window height is too short

--- project.js
+++ project.js
@@ -3,14 +3,19 @@ let secondInput = document.getElementById('second-input');
 
 let output = document.getElementById('output');
 
+function jarvis_says(x){
+    output.innerHTML = "Jarvis says: " + x;
+}
+
+
 function addition(){
     // we weren't taught how to typecast in class, so I decided to typecast using Number from W3Schools, and used the function isNaN to catch non valid inputs.
     output.style.color = "black";
     if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
-        output.innerHTML = "Invalid input(s)";
+        jarvis_says("Invalid input(s)");
     }else{
         let x = Number(firstInput.value) + Number(secondInput.value);
-        output.innerHTML = x;
+        jarvis_says(x);
         if (x < 0) output.style.color = "red";
     }
 }
@@ -18,10 +23,10 @@ function addition(){
 function subtraction(){
     output.style.color = "black";
     if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
-        output.innerHTML = "Invalid input(s)";
+        jarvis_says("Invalid input(s)");
     }else{
         let x = Number(firstInput.value) - Number(secondInput.value);
-        output.innerHTML = x;
+        jarvis_says(x);
         if (x < 0) output.style.color = "red";
     }
 }
@@ -29,10 +34,10 @@ function subtraction(){
 function multiplication(){
     output.style.color = "black";
     if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
-        output.innerHTML = "Invalid input(s)";
+        jarvis_says("Invalid input(s)");
     }else{
         let x = Number(firstInput.value) * Number(secondInput.value);
-        output.innerHTML = x;
+        jarvis_says(x);
         if (x < 0) output.style.color = "red";
     }
 }
@@ -40,12 +45,12 @@ function multiplication(){
 function division(){
     output.style.color = "black";
     if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
-        output.innerHTML = "Invalid input(s)";
+        jarvis_says("Invalid input(s)");
     }else if (Number(secondInput.value === 0)){
-        output.innerHTML = "Divide by 0";
+        jarvis_says("Divide by 0");
     }else{
         let x = Number(firstInput.value) / Number(secondInput.value);
-        output.innerHTML = x;
+        jarvis_says(x);
         if (x < 0) output.style.color = "red";
 
     }
@@ -55,20 +60,20 @@ function division(){
 function power(){
     output.style.color = "black"; //reset color of font
     if (firstInput.value === '' || secondInput.value === '' || isNaN(Number(firstInput.value))|| isNaN(Number(secondInput.value))){
-        output.innerHTML = "Invalid input(s)";
+        jarvis_says("Invalid input(s)");
     }else if (Number(secondInput.value >= 0)){
         let n = 1;
         for (let i = 0; i < Number(secondInput.value); i++){
             n *= Number(firstInput.value);
         }
-        output.innerHTML = n;
+        jarvis_says(n);
         if (n < 0) output.style.color = "red";
     }else{
         let n = 1;
         for (let i = 0; i > Number(secondInput.value); i--){
             n /= Number(firstInput.value);
         }
-        output.innerHTML = n;
+        jarvis_says(n);
         if (n < 0) output.style.color = "red";
     }
 }
@@ -76,5 +81,5 @@ function power(){
 function clearInput(){
     firstInput.value = '';
     secondInput.value= '';
-    output.innerHTML = '';
+    jarvis_says('');
 }
